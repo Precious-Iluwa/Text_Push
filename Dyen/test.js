3783d232e95d49402b1817089bcbe9255e2b0059
Codez <118695769+Precious-Iluwa@users.noreply.github.com>
2024-05-26 17:11:10 -0700

    Add files via upload

---
+++ Dyen/test.js
@@ -0,0 +1,19 @@
+
+// THIS IS THE FIRST TASK
+var b;
+
+for(b=1; b < 5; b++){
+    b++
+    console.log(b + ' is an even number');
+}
+
+// THIS IS THE SECOND TASK
+// var b;
+// let arrayName = ['ball', 'pen', 'egg', 'chair', 'paper', 'laptop'];
+
+// for(b=0; b <= arrayName.length; b++){
+//     console.log(arrayName[b]);
+// }
+
+// PS: If u want to try out any block of code make sure u comment the first one out before trying it out so as to avoid an UNCAUGHT ERROR ON THE CONSOLE. 
+
